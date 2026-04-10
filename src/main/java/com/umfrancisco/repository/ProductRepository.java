d1b6873e076b7709c6721357c95669d15c3aaabb
umfrancisco <francis.guitler@gmail.com>
2026-04-10 09:52:36 -0300

    Remove unused imports

--- src/main/java/com/umfrancisco/repository/ProductRepository.java
+++ src/main/java/com/umfrancisco/repository/ProductRepository.java
@@ -1,11 +1,6 @@
 package com.umfrancisco.repository;
 
 import java.util.List;
-
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 
 import com.umfrancisco.model.Product;

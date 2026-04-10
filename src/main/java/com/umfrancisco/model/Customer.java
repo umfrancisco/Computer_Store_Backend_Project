079169c13a0656b49d8f27bde9227a1c515dc6ef
umfrancisco <francis.guitler@gmail.com>
2026-04-10 10:07:14 -0300

    Removed unused imports

--- src/main/java/com/umfrancisco/model/Customer.java
+++ src/main/java/com/umfrancisco/model/Customer.java
@@ -1,11 +1,7 @@
 package com.umfrancisco.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 
 @Entity
 public class Customer {
